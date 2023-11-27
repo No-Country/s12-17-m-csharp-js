dfd0d9ac6015643c8fa84d2d0c0230fd2f66b5f5
LeanCano <leandrocano24@gmail.com>
2023-11-27 17:32:06 -0500

    fixregisterform

--- Frontend/ecommerce/app/components/Form/Layout.jsx
+++ Frontend/ecommerce/app/components/Form/Layout.jsx
@@ -4,11 +4,11 @@ import TextInput from "./TextInput"
 
 const Layout = ({ children }) => {
     return (
-        <div className="flex h-screen">
+        <div className="flex h-full">
             {/* Columna del logo */}
             <div className="w-1/2 bg-primary flex items-center justify-center">
-                <div className=" flex items-center justify-center">
-                    <h1 className="text-white text-4xl font-bold">Logo</h1>
+                <div className=" flex items-center justify-center h-full">
+                    <h1 >Logo</h1>
                 </div>
             </div>
 
