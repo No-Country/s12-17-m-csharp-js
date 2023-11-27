4254a01eaa545510b3127a1391f92728702ec3c9
LeanCano <leandrocano24@gmail.com>
2023-11-27 18:17:49 -0500

    LoginStructure

--- Frontend/ecommerce/app/components/Form/TextInput.jsx
+++ Frontend/ecommerce/app/components/Form/TextInput.jsx
@@ -7,7 +7,7 @@ const TextInput = ({ name, required = false, type, ...props }) => {
             <input
                 {...props}
                 type={type}
-                className="w-full border border-gray-300 rounded-md px-3 py-2"
+                className="w-full border-b border-gray-300 rounded-none px-3 py-2 focus:outline-none focus:border-b-2 focus:border-blue-500"
             />
         </div>
     )
--- Frontend/ecommerce/app/register/page.jsx
+++ Frontend/ecommerce/app/register/page.jsx
@@ -8,7 +8,7 @@ import { MdFacebook } from "react-icons/md";
 const page = () => {
     return (
         <Layout>
-           <div className="mt-64 mb-20"><h1 className="text-center font-bold  ">Regístrate para ser usuario</h1>
+           <div className="mt-20 mb-20"><h1 className="text-center font-bold  ">Regístrate para ser usuario</h1>
            </div> 
            <div >
                 <TextInput name="Nombres" required />
@@ -16,7 +16,7 @@ const page = () => {
                 <TextInput name="Email" type="email" required />
                 <TextInput name="Contraseña" type="password" required />
                 <TextInput name="Confirmar contraseña" type="password" required />
-                <div className="mb-4">
+                <div className="mt-10 mb-10">
                     <label className="flex items-center">
                     <input type="checkbox" className="mr-2" required />
                         He leido y acepto los términos de servicio y nuestra politica de privacidad
@@ -25,7 +25,7 @@ const page = () => {
                 <Button >Crear cuenta</Button>
                 <SocialButton> <p className="flex items-center justify-center"><FaGoogle className="mr-2" /> Continuar con Google</p> </SocialButton>
                 <SocialButton> <p className="flex items-center justify-center"><MdFacebook className="mr-2"/>Continuar con Facebook</p></SocialButton>
-                <p className="text-center mt-4">
+                <p className="text-center mt-10 mb-8">
                         ¿Ya tienes contraseña? <a href="#">Ingresa</a>
                 </p>
                 
