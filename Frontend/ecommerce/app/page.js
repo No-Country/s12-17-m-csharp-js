e81c4725f1e6732bc6da80ad23698575920dbaf4
LeanCano <111925428+LeanCano@users.noreply.github.com>
2023-11-30 17:11:22 +0000

    Images Carousel 1

--- Frontend/ecommerce/app/page.js
+++ Frontend/ecommerce/app/page.js
@@ -3,17 +3,15 @@ import PopUp from './components/Popup/PopUp'
 
 export default function Home() {
   let slides = [
-    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
-    "https://wallpapercave.com/wp/wp3386769.jpg",
-    "https://wallpaperaccess.com/full/809523.jpg",
-    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
+    "/megasale1.png",
+    "/megasale2.png",
+    "/megasale3.png",
   ];
 
   let secondSlides= [
-    "https://i.pinimg.com/originals/51/82/ac/5182ac536727d576c78a9320ac62de30.jpg",
-    "https://wallpapercave.com/wp/wp3386769.jpg",
-    "https://wallpaperaccess.com/full/809523.jpg",
-    "https://getwallpapers.com/wallpaper/full/5/c/0/606489.jpg",
+    "/specialoffer.png",
+    "/specialoffer2.png",
+    
   ];
 
   let thirdSlides= [
