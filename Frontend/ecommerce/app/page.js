ebee64533515040e5a8cc22051a5377218783b4e
LeanCano <111925428+LeanCano@users.noreply.github.com>
2023-11-30 03:00:27 +0000

    algo

--- Frontend/ecommerce/app/page.js
+++ Frontend/ecommerce/app/page.js
@@ -3,7 +3,7 @@ import PopUp from './components/Popup/PopUp'
 export default function Home() {
   return (
     <main>
-      <PopUp open title={'Registro Exitoso!'} description={'Revisa tu email para validar tu cuenta.'} />
+      hello
     </main>
   )
 }
