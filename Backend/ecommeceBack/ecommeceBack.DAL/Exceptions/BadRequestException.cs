6d07d314cb72a59802cf0e2d75726107b9c12c1f
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-12-04 12:33:09 -0300

    Configuración de automapper y creacion de excepciones personalizadas

---
+++ Backend/ecommeceBack/ecommeceBack.DAL/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace ecommeceBack.API.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException(string? message) : base(message)
+        {
+        }
+    }
+}
