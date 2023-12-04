1415fa518584ea145de6d2b4734c52e00eddb629
rodriarguello <ds21.tn.arguello.rodrigo@gmail.com>
2023-12-04 12:30:52 -0300

    Creación de IGenericService

---
+++ Backend/ecommeceBack/ecommeceBack.BLL/contrato/IGenericService.cs
@@ -0,0 +1,23 @@
+﻿using ecommeceBack.Models.Entidades;
+using ecommeceBack.Models.VModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ecommeceBack.BLL.contrato
+{
+    public interface IGenericService<DTOCreacion,DTO>
+    {
+        Task<DTO> Registrar(DTOCreacion modelo);
+
+        Task<DTO> Actualizar(int id, DTOCreacion modelo);
+
+        Task<bool> Eliminar(int id);
+
+        Task<DTO> ObtenerPorId(int id);
+
+        Task<IEnumerable<DTO>> ObtenerTodos();
+    }
+}
