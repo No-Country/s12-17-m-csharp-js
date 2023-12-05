e85bb712d7aa2ec9c913602668f65ed3b009ac6f
LeanCano <111925428+LeanCano@users.noreply.github.com>
2023-12-05 12:38:42 +0000

    vender producto paso 1

--- Frontend/ecommerce/app/control_panel/products/create/choose_category_step/page.jsx
+++ Frontend/ecommerce/app/control_panel/products/create/choose_category_step/page.jsx
@@ -2,32 +2,24 @@ const ChooseCategoryStepPage = () => {
   return (
     <div>
       <h1 className="text-3xl font-semibold mb-4">¿Qué producto vas a vender?</h1>
-      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Commodi quisquam
-      laboriosam obcaecati architecto necessitatibus ullam natus, maxime
-      maiores, quo neque cum vero doloremque illum molestiae minima perspiciatis
-      numquam amet! Est! Facilis aspernatur fugiat sapiente aliquid maxime
-      necessitatibus porro alias cupiditate sit. Eum fugiat ipsum repudiandae
-      dolores aliquid quisquam! Quisquam consequuntur velit ipsum consectetur
-      quis excepturi voluptate vero, harum illo! Magnam! Consequuntur ab
-      molestiae voluptate, enim explicabo et facilis minima minus provident?
-      Laborum ea, consequatur minus modi praesentium a nisi dolore dolorum
-      saepe. Nihil corporis error nam laboriosam rerum, omnis optio. Eaque ut
-      tempore mollitia quia velit maxime aperiam facere placeat? Voluptatibus
-      quo doloremque temporibus, quasi iste molestias tenetur debitis facere
-      corporis sed. Facilis necessitatibus voluptatem dolorem, maxime illo
-      pariatur atque. Officia libero quisquam vero id! Earum culpa ratione at
-      assumenda iste quas totam autem nesciunt exercitationem perspiciatis ipsum
-      voluptates, repellendus et distinctio, fugiat beatae. Tenetur quos
-      corrupti quaerat facilis consectetur! Deserunt, nam libero consectetur ab
-      recusandae a quidem neque veniam incidunt minus nemo accusamus iure.
-      Perspiciatis nostrum error, optio est illum numquam excepturi mollitia
-      corporis accusantium ad facere nam velit! Earum corrupti quibusdam nostrum
-      ipsum numquam quia labore, quo omnis dolor aspernatur neque aut vero unde?
-      Obcaecati expedita officia aut fugiat et reiciendis! Doloribus at, quis
-      odit officia repellat vero! Ullam, laboriosam suscipit? Nulla ex atque
-      fugit nobis commodi sit sint odio! Ad pariatur vel animi dicta voluptate a
-      dolorem assumenda saepe illo cum. Excepturi consequuntur corporis quisquam
-      enim dolorem.
+      <div className="flex justify-between p-4 mt-28">
+        <div className="category flex flex-col items-center bg-secondary rounded-lg p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:cursor-pointer">
+          <img src="/vehiculos.png" alt="Vehículos" className="w-24 h-24 mb-2 rounded-full" />
+          <p className="text-center font-semibold">Vehículos</p>
+        </div>
+        <div className="category flex flex-col items-center bg-secondary  rounded-lg p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:cursor-pointer">
+          <img src="/productos.png" alt="Productos" className="w-24 h-24 mb-2 rounded-full" />
+          <p className="text-center font-semibold">Productos</p>
+        </div>
+        <div className="category flex flex-col items-center bg-secondary  rounded-lg p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:cursor-pointer">
+          <img src="/inmuebles.png" alt="Inmuebles" className="w-24 h-24 mb-2 rounded-full" />
+          <p className="text-center font-semibold">Inmuebles</p>
+        </div>
+        <div className="category flex flex-col items-center bg-secondary  rounded-lg p-4 transition duration-300 ease-in-out transform hover:scale-105 hover:cursor-pointer">
+          <img src="/servicios.png" alt="Servicios" className="w-24 h-24 mb-2 rounded-full" />
+          <p className="text-center font-semibold">Servicios</p>
+        </div>
+      </div>
     </div>
   );
 };
