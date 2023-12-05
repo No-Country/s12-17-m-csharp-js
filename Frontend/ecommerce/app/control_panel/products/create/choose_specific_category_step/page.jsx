6f4141162ebca7aa633ab1fd5837b8abd4c95c43
LeanCano <111925428+LeanCano@users.noreply.github.com>
2023-12-05 14:28:13 +0000

    NC-72

--- Frontend/ecommerce/app/control_panel/products/create/choose_specific_category_step/page.jsx
+++ Frontend/ecommerce/app/control_panel/products/create/choose_specific_category_step/page.jsx
@@ -1,12 +1,19 @@
+import { IoIosArrowForward } from "react-icons/io";
 const ChooseSpecificCategoryStepPage = () => {
     return (
-      <div>
+        <div>
         <h1 className="text-3xl font-semibold mb-2">Clasifícalo con una categoría</h1>
         <div className="font-semibold mt-8 mb-2">Categorías</div>
         <ul className="flex flex-col border-b border-gray-200 mb-4">
-          <li className="mb-4 cursor-pointer bg-pink-200 rounded-lg p-2">Gaming</li>
-          <li className="mb-4 cursor-pointer bg-pink-200 rounded-lg p-2">Hogar</li>
-          <li className="mb-4 cursor-pointer bg-pink-200 rounded-lg p-2">Celulares</li>
+          <li className="mb-4 cursor-pointer bg-pink-200 rounded-lg p-2 flex items-center justify-between">
+            Gaming <IoIosArrowForward  />
+          </li>
+          <li className="mb-4 cursor-pointer bg-pink-200 rounded-lg p-2 flex items-center justify-between">
+            Hogar <IoIosArrowForward  />
+          </li>
+          <li className="mb-4 cursor-pointer bg-pink-200 rounded-lg p-2 flex items-center justify-between">
+            Celulares <IoIosArrowForward  />
+          </li>
           {/* Puedes añadir más pestañas aquí */}
         </ul>
         {/* Agrega más contenido para otras pestañas si es necesario */}
