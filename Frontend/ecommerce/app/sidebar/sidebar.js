b7814e1b1e3e3d028708992b9e727960b8dc5da7
LeanCano <111925428+LeanCano@users.noreply.github.com>
2023-12-11 18:19:46 +0000

    sidebar with filters

--- Frontend/ecommerce/app/sidebar/sidebar.js
+++ Frontend/ecommerce/app/sidebar/sidebar.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Sidebar = () => {
   return (
-    <div className="bg-gray-200 w-64">
+    <div className="bg-white-200 w-64 mr-4">
       <div className="p-4">
-        <h2 className="text-xl font-semibold mb-4">Categorías</h2>
-        <ul>
-          <li className="mb-2"><a href="#" className="text-blue-500 hover:underline">Electrónicos</a></li>
-          <li className="mb-2"><a href="#" className="text-blue-500 hover:underline">Ropa</a></li>
-          <li className="mb-2"><a href="#" className="text-blue-500 hover:underline">Hogar y Jardín</a></li>
-          {/* Agrega más elementos de la barra lateral según necesites */}
-        </ul>
+        <h2 className="text-xl font-semibold mb-16 mt-8">Filtrar</h2>
+
+        <div className="mb-4">
+          <h3 className="font-semibold mb-2">Categoría</h3>
+          <select className="p-2 mb-2">
+            <option value="">Todas</option>
+            <option value="muebles">Muebles</option>
+            <option value="celulares">Celulares</option>
+            <option value="gaming">Gaming</option>
+            {/* Agrega más opciones de categoría según necesites */}
+          </select>
+        </div>
+
+        <div className="mb-4">
+          <h3 className="font-semibold mb-2">Condición</h3>
+          <select className="p-2 mb-2">
+            <option value="">Todas</option>
+            <option value="nuevo">Nuevo</option>
+            <option value="usado">Usado</option>
+          </select>
+        </div>
+
+        <div>
+          <h3 className="font-semibold mb-2">Rango de precios</h3>
+          <select className="p-2">
+            <option value="">Todos</option>
+            <option value="0-50">$0 - $50</option>
+            <option value="50-100">$50 - $100</option>
+            <option value="100-200">$100 - $200</option>
+            {/* Agrega más opciones de rango de precios según necesites */}
+          </select>
+        </div>
       </div>
     </div>
   );
+
 };
 
 export default Sidebar;
