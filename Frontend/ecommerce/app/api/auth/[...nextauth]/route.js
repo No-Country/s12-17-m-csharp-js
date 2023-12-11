5a81351733cefe2908fdb392958ca61f4e488b48
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2023-12-11 11:35:23 -0500

    Fix authOptions type

--- Frontend/ecommerce/app/api/auth/[...nextauth]/route.js
+++ Frontend/ecommerce/app/api/auth/[...nextauth]/route.js
@@ -1,39 +1,5 @@
 import NextAuth from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import { loginUser } from "@/app/utils/api";
-
-export const authOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text", placeholder: "jsmith" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials, req) {
-        console.log(credentials);
-
-        const token = await loginUser({
-          email: credentials.email,
-          password: credentials.password,
-        });
-
-        console.log(token);
-        if (token) {
-          return {
-            id: "1",
-            name: "Test User",
-            email: credentials.email,
-          };
-        }
-        return null;
-      },
-    }),
-  ],
-  pages: {
-    signIn: "/login",
-  },
-};
+import { authOptions } from "@/lib/authOptions";
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
