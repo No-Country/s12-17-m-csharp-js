63b643f44c5939ce74632f1c12e0ccd45af73138
Rafael Cangahuala <78826890+rcpc265@users.noreply.github.com>
2023-12-13 12:32:55 -0500

    Update PopUp functionality

--- Frontend/ecommerce/app/components/Form/TextAreaInput.jsx
+++ Frontend/ecommerce/app/components/Form/TextAreaInput.jsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import IsRequired from './IsRequired'
+import React from "react";
+import IsRequired from "./IsRequired";
 
-const TextAreaInput = ({ label, placeholder,  isRequired = false, messageValidation = null }) => {
-    return (
-        <div className="space-y-2">
+const TextAreaInput = ({
+  label,
+  placeholder,
+  isRequired = false,
+  messageValidation = null,
+}) => {
+  return (
+    <div className="space-y-2">
+      <p className="font-semibold  text-lg">
+        <IsRequired isRequired={isRequired}>{label}</IsRequired>
+      </p>
 
-            <p className="font-bold  text-xl">
-                <IsRequired isRequired={isRequired}>
-                    {label}
-                </IsRequired>
-            </p>
+      <textarea
+        className="text-gray-800 px-4 py-3 rounded-md w-full bg-black/10"
+        placeholder={placeholder}
+        name=""
+        id=""
+        rows={4}
+      ></textarea>
 
-            <textarea placeholder={placeholder} name="" id="" cols={30} rows={10} className={`text-gray-800 px-4 py-3 rounded-md w-full bg-black/10`}></textarea>
+      {messageValidation ? (
+        <p className="text-sm text-[#696969] w-4/6">{messageValidation}</p>
+      ) : null}
+    </div>
+  );
+};
 
-            {
-                messageValidation ?
-                    <p className='text-sm text-[#696969] w-4/6' >
-                        {messageValidation}
-                    </p>
-                : 
-                    null
-            }
-        </div>
-    )
-}
-
-export default TextAreaInput
+export default TextAreaInput;
