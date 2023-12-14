e26c30fbe6654a13fcc476373434be0208e46425
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-12-13 22:11:56 -0300

    Carrito

--- Frontend/ecommerce/app/cart/page.jsx
+++ Frontend/ecommerce/app/cart/page.jsx
@@ -20,6 +20,11 @@ const CartPage = () => {
     useStore.getState().removeAllFromCart();
   };
 
+  const handleRemoveOneItem = productId => {
+    // Lógica para eliminar uno de los productos del carrito
+    useStore.getState().removeFromCart(productId);
+  };
+
   const handleCheckout = () => {
     // Lógica para continuar con el proceso de compra
     // Redireccionar a la página de checkout u otro lugar
@@ -33,30 +38,42 @@ const CartPage = () => {
       <div className=' bg-gradient-to-b from-[#026048] to-[#FEAF00] h-[456px] absolute w-full'></div>
     <div className="container mx-auto px-4 py-8 relative">
       <h1 className="text-2xl mb-4 text-[#FEAF00]">Carrito de Compras</h1>
-      <h3 className="text-l mb-4 text-[#FEAF00]">Continuar Comprando</h3>
-      <div className=" min-h-[600px] bg-[#EAEAEA] rounded-[20px] max-w-[916px]">
-        <div className=' flex text-black text-xl font-bold'>
-          <p className=' w-2/5 m-4'>Producto</p>
-          <p className=' w-1/6 m-4'>Precio</p>
-          <p className=' w-1/6 m-4'>Cantidad</p>
-          <p className=' w-1/6 m-4'>Total</p>
+      <h3 className="text-xl mb-4 text-[#FEAF00] flex"><img src='assets/arrow.svg' className='mr-4' ></img> Continuar Comprando</h3>
+      <div className='flex'>
+
+      <div className=" min-h-[600px] bg-[#EAEAEA] rounded-[20px] w-[916px] mt-20">
+        <div className=' flex text-black text-xl font-bold h-[60px] items-center'>
+          <p className=' w-2/5 ml-3'>Producto</p>
+          <p className=' w-1/6 ml-3'>Precio</p>
+          <p className=' w-1/6 ml-3'>Cantidad</p>
+          <p className=' w-1/6 ml-3'>Total</p>
         </div>
         {cart.map(item => (
-          <div key={item.id} className="border border-gray-300 p-4">
-            <p className="font-semibold">{item.title}</p>
-            <p>${item.price}</p>
-            <p>Cantidad: {item.quantity}</p>
-            <div className="flex justify-between mt-2">
-              <button onClick={() => handleRemoveOne(item.id)} className="bg-red-500 text-white px-2 rounded">-</button>
-              <button onClick={() => handleAddOne(item.id)} className="bg-green-500 text-white px-2 rounded">+</button>
+          <div key={item.id} className="border border-gray-300 p-2 flex h-[160px] items-center">
+            <p className="font-semibold w-2/5 m-2">{item.title}</p>
+            <p className=' w-1/6 m-2'>${item.price}</p>
+            <div className="flex justify-between w-1/6">
+              <button onClick={() => handleRemoveOne(item.id)} className="bg-[#EAEAEA] text-black border border-black px-2 mx-4 rounded h-7 w-7">-</button>
+            <p>{item.quantity}</p>
+              <button onClick={() => handleAddOne(item.id)} className="bg-[#EAEAEA] text-black border border-black px-2 mx-4 rounded h-7 w-7">+</button>
             </div>
+           <p className=' w-1/6 m-2'>${item.quantity * item.price}</p>
+           <button onClick={() => handleRemoveOneItem(item.id)}> <img src='assets/trash.svg' ></img></button>           
           </div>
         ))}
       </div>
+          <div className='w-[350px] h-[150px] bg-[#FEAF00] ml-10 mt-60'>
+            <div className='flex justify-between p-8 text-xl'>
+        <p className="font-semibold">Total:</p>
+        <p className="font-semibold">${Math.round(total*100)/100}</p>
+
+            </div>
+        {/* <button onClick={handleRemoveAll} className="bg-black text-white px-4 py-2 ">Eliminar Todos</button> */}
+        <button onClick={handleCheckout} className="bg-black text-white w-[90%] flex mx-auto justify-center h-[46px] items-center">Continuar con la Compra</button>
+
+          </div>
+      </div>
       <div className="flex justify-between mt-8">
-        <button onClick={handleRemoveAll} className="bg-red-500 text-white px-4 py-2 rounded">Eliminar Todos</button>
-        <p className="font-semibold">Total: ${total}</p>
-        <button onClick={handleCheckout} className="bg-green-500 text-white px-4 py-2 rounded">Continuar con la Compra</button>
       </div>
     </div>
         </div>
