f858449adb4781320999b86fc3ba73590f8af1ba
Emanuel-Capo <emanuel_c400@hotmail.com>
2023-12-19 11:40:48 -0300

    aplicar reglas de formato orderService

--- Frontend/ecommerce/services/orderService.js
+++ Frontend/ecommerce/services/orderService.js
@@ -1,23 +1,22 @@
 import apiClient from "./apiClient";
 
 class OrderService {
-    postOrder(data) {
-        return new Promise((resolve, reject) => {
-            apiClient
-                .post('pedidos', data)
-                // .then((response) => response.json())
-                .then((response) => {
-                    console.log(response);
-                    // recibir el resultado del pago
-                    resolve();
-                })
-                .catch((error) => {
-                    // manejar la respuesta de error al intentar crear el pago
-                    console.log(error);
-                    reject();
-                });
+  postOrder(data) {
+    return new Promise((resolve, reject) => {
+      apiClient
+        .post("pedidos", data)
+        // .then((response) => response.json())
+        .then((response) => {
+          // recibir el resultado del pago
+          resolve();
+        })
+        .catch((error) => {
+          // manejar la respuesta de error al intentar crear el pago
+          console.log(error);
+          reject(error);
         });
-    }
+    });
+  }
 }
 
 const orderService = new OrderService();
